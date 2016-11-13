b5d4efa41245331357971200fc7561ac8368a651
Bodhert <alejandro.cordoba.bodhert@gmail.com>
2016-11-13 01:27:14 -0500

    registra los objetos

--- Usuario/EncontraloPues-User/src/pages/agregarobjts/agregarobjts.ts
+++ Usuario/EncontraloPues-User/src/pages/agregarobjts/agregarobjts.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, Platform, Alert } from 'ionic-angular';
+import { NavController, Platform, Alert,AlertController } from 'ionic-angular';
+import { IngresarObjetos } from '../../providers/ingresar-objetos';
+import { AuthService } from '../../services/auth/auth.service';
+
 
 /*
   Generated class for the Agregarobjts page.
@@ -8,21 +11,33 @@ import { NavController, Platform, Alert } from 'ionic-angular';
   Ionic pages and navigation.
 */
 @Component({
-  selector: 'page-agregarobjts',
-  templateUrl: 'agregarobjts.html'
+  selector: 'page-agregarobjts' ,
+  templateUrl: 'agregarobjts.html' ,
+  providers: [ IngresarObjetos ] ,
 })
+
 export class Agregarobjts {
 
    tag: any;
    public tags: Array<String>;
    descripcionOculta: string;
+   resulConsulta: any;
+   correoUsuario: string;
+   usuario: any;
 
-  constructor(public navCtrl: NavController,public platform: Platform) {
+  constructor(public navCtrl: NavController,public platform: Platform ,  
+              private ingresarService: IngresarObjetos,public auth: AuthService,
+              private alertCtrl: AlertController) {
      this.tags = [];
+     this.usuario = auth.user;
+     this.correoUsuario = this.usuario.email;
+     //modificar correoUsuario al correo que me da auth0
   }
 
   ionViewDidLoad() {
     console.log('Hello Agregarobjts Page');
+    console.log(this.correoUsuario);
+    
   }
 
     public addTag(tagNameInput: any): void {
@@ -41,5 +56,33 @@ export class Agregarobjts {
     // Delete the tag in that index
     this.tags.splice(index, 1);
   } 
+
+  public confirmar(){
+
+    if(this.tags.length > 0){
+      
+      let registro = {
+        tags: this.tags,
+        correoUsuario: this.correoUsuario
+      };
+
+      this.ingresarService.insertarObjetos(registro).subscribe(data => {
+        this.resulConsulta = data;
+        console.log(this.resulConsulta);
+
+        if(this.resulConsulta.correcto){
+          alert(this.resulConsulta.mensaje);
+          console.log(this.resulConsulta.mensaje);
+          this.tags = [];
+        }else{
+          alert(this.resulConsulta.mensaje);
+        }
+
+      });
+
+    }else{
+      alert("los campos deben de estar completos");
+    }
+  }
 
 }

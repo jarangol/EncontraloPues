8d890f0818a42a0cc9e78d1fb42896c6e3ac385e
Bodhert <alejandro.cordoba.bodhert@gmail.com>
2016-11-17 18:01:36 -0500

    insertando auth0 a base de datos

---
+++ Usuario/EncontraloPues-User/src/providers/usuarioprovider.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+/*
+  Generated class for the Usuarioprovider provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class Usuarioprovider {
+  private data: any;
+  private serverURL = 'https://afternoon-crag-97293.herokuapp.com';
+
+  constructor(public http: Http) {
+    console.log('Hello Usuarioprovider Provider');
+  }
+
+  public ingresarUsuario(consulta){
+    let body = JSON.stringify(consulta);
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    this.data = this.http.post(this.serverURL + '/api/registrarUsuarioAuth0', body,
+      { headers: headers }).map(res => res.json());
+
+    return this.data;
+  }
+
+}
